136297e5928c4de797d29a3fff5880beafd6cd8a
4python@ai <4python@ai>
2025-05-28 12:15:58 +0800

    径向菜单

--- pages/addGame/addGame.js
+++ pages/addGame/addGame.js
@@ -12,6 +12,7 @@ Page({
 
     onLoad: function () {
         // 页面加载时自动显示菜单
+        this.toggleMenu();
     },
 
     // 处理菜单项点击
@@ -68,8 +69,40 @@ Page({
 
     // 切换菜单显示状态
     toggleMenu() {
+        const isOpen = !this.data.isMenuOpen;
+
+        if (isOpen) {
+            // 展开菜单时的卫星动画
+            const points = ['point1', 'point2', 'point3', 'point4'];
+            points.forEach((point, index) => {
+                const animation = wx.createAnimation({
+                    duration: 300,
+                    timingFunction: 'ease-out',
+                    delay: index * 80  // 每个菜单项延迟80ms
+                });
+
+                // 从中心弹出效果
+                animation.scale(0).opacity(0).step();
+                animation.scale(1).opacity(1).step();
+
+                this.setData({
+                    [`animations.${point}`]: animation.export()
+                });
+            });
+        } else {
+            // 关闭菜单时恢复初始状态
+            const resetAnim = wx.createAnimation({ duration: 0 });
+            resetAnim.scale(1).opacity(1).step();
+
+            ['point1', 'point2', 'point3', 'point4'].forEach(point => {
+                this.setData({
+                    [`animations.${point}`]: resetAnim.export()
+                });
+            });
+        }
+
         this.setData({
-            isMenuOpen: !this.data.isMenuOpen
+            isMenuOpen: isOpen
         });
     }
 });
