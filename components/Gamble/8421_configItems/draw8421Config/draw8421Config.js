ba72a8a927c09fdd23ca305c2bbfee68be016852
4python@ai <4python@ai>
2025-07-11 18:51:45 +0800

    done 888421

--- components/Gamble/8421_configItems/draw8421Config/draw8421Config.js
+++ components/Gamble/8421_configItems/draw8421Config/draw8421Config.js
@@ -16,32 +16,73 @@ Component({
       '得分1分以内',
       '无顶洞'
     ],
-    selected: 0
+    selected: 0,
+    // 分数选择器相关
+    diffScores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    selectedDiffScore: 1
   },
   attached() {
     // 组件初始化时，根据store中的值设置选中状态
     this.syncSelectedFromStore();
+    // 更新显示选项
+    this.updateDisplayOptions();
   },
 
   methods: {
     syncSelectedFromStore() {
       const currentValue = G_4P_8421_Store.draw8421Config;
       if (currentValue) {
-        const index = this.data.options.findIndex(option => option === currentValue);
-        if (index !== -1) {
-          this.setData({ selected: index });
+        if (currentValue === 'DrawEqual') {
+          this.setData({ selected: 0 });
+        } else if (currentValue.startsWith('Diff_')) {
+          // 解析分数值
+          const score = parseInt(currentValue.replace('Diff_', ''));
+          this.setData({
+            selected: 1,
+            selectedDiffScore: score || 1
+          });
+        } else if (currentValue === 'NoDraw') {
+          this.setData({ selected: 2 });
         }
       }
     },
 
-    onSelect(e) {
-      this.setData({ selected: e.currentTarget.dataset.index });
+    updateDisplayOptions() {
+      const score = this.data.selectedDiffScore;
+      const newDisplayOptions = [...this.data.displayOptions];
+      newDisplayOptions[1] = `得分${score}分以内`;
+      this.setData({ displayOptions: newDisplayOptions });
     },
+
+    onSelect(e) {
+      const index = e.currentTarget.dataset.index;
+      this.setData({ selected: index });
+    },
+
+    // 分数选择器相关方法
+    onDiffScoreChange(e) {
+      const selectedIndex = e.detail.value;
+      const selectedScore = this.data.diffScores[selectedIndex];
+      this.setData({ selectedDiffScore: selectedScore });
+      this.updateDisplayOptions();
+      console.log('选择分数:', selectedScore);
+    },
+
+
     onCancel() {
       this.triggerEvent('cancel');
     },
     onConfirm() {
-      const selectedValue = this.data.options[this.data.selected];
+      let selectedValue = '';
+
+      // 根据选择的选项生成配置值
+      if (this.data.selected === 0) {
+        selectedValue = 'DrawEqual';
+      } else if (this.data.selected === 1) {
+        selectedValue = `Diff_${this.data.selectedDiffScore}`;
+      } else if (this.data.selected === 2) {
+        selectedValue = 'NoDraw';
+      }
 
       // 调用store的action更新数据
       G_4P_8421_Store.updateDingdongRule(selectedValue);
