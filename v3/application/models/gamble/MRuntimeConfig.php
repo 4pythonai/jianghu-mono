31c1a829547792bd2711fa6cf0b62805cb654f6e
4python@ai <4python@ai>
2025-07-11 12:31:23 +0800

    去除硬编码

--- v3/application/models/gamble/MRuntimeConfig.php
+++ v3/application/models/gamble/MRuntimeConfig.php
@@ -5,6 +5,25 @@ if (!defined('BASEPATH')) {
 }
 class MRuntimeConfig extends CI_Model {
 
+    // 原始配置缓存
+    private $_gambleConfigCache = [];
+
+    public function getGambleConfig($gambleid) {
+        // 如果缓存中有，直接返回
+        if (isset($this->_gambleConfigCache[$gambleid])) {
+            return $this->_gambleConfigCache[$gambleid];
+        }
+
+        // 从数据库获取
+        $this->db->where('id', $gambleid);
+        debug(" **************** 数据库操作");
+        $GambleConfigRow = $this->db->get('t_game_gambles')->row_array();
+
+        // 缓存结果
+        $this->_gambleConfigCache[$gambleid] = $GambleConfigRow;
+        return $GambleConfigRow;
+    }
+
     // 8421 配置缓存
     private $_8421ConfigsCache = [];
 
@@ -30,71 +49,44 @@ class MRuntimeConfig extends CI_Model {
         return $this->_8421ConfigsCache[$gambleid];
     }
 
-
-
-    // 肉的分值
-    // 肉算 X 分, 吃肉数量由上面表格决定,考虑封顶
-    // 分值翻倍:本洞赢 8 分,  吃 1 个洞2倍(16 分) ,2 个洞 X3(24 分),3 个洞 X4 倍(32 分).此时如果有封顶 如 3,则为 8+N*3
-    // 连续翻倍: 吃完所有的洞, 2,4,8,16,不考虑封顶,也不考虑成绩表
-
-
     // 可以吃肉的范围, 赢方最好成绩----对应吃肉数量
     public function get8421EatingRange($gambleid) {
-
-        // 成绩越好，能吃的肉越多
-        // BetterThanBirdie: Eagle、Albatross等（比小鸟球更好）
-        // Birdie: 小鸟球
-        // Par: 标准杆 
-        // WorseThanPar: Bogey及以上（比Par更差）
-        return [
-            'BetterThanBirdie' => 2,     // Eagle等，比小鸟球更好，能吃最多肉
-            'Birdie' => 2,               // 小鸟球，能吃2块肉
-            'Par' => 1,                  // 标准杆，能吃1块肉
-            'WorseThanPar' => 0,         // Bogey及以上，不能吃肉
-        ];
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['eating_range']) && !empty($config['eating_range'])) {
+            return json_decode($config['eating_range'], true);
+        }
     }
 
     public function getMeatValueConfigString($gambleid) {
-        return "MEAT_AS_2"; // 每块肉3分，吃肉数量由上面表格(get8421EatingRange)决定,考虑封顶,每个洞只能吃1次
-        // return "SINGLE_DOUBLE"; //  分值翻倍翻倍,比如:本洞赢 8 分,  吃 1 个洞2倍(16 分) ,2 个洞 X3(24 分),3 个洞 X4 倍(32 分).此时如果有封顶 如 3,则为 8+N*3,吃肉数量由上面表格(get8421EatingRange)决定,每个洞只能吃1次
-        // return "CONTINUE_DOUBLE"; // 连续翻倍,会吃掉当前所有可以吃的肉,不遗留任何肉,无需考虑封顶,无需考虑 get8421EatingRange,每个洞只能吃1次
+        $config = $this->getGambleConfig($gambleid);
+        return $config['meat_value_config_string'];
     }
 
     // 每次吃肉的值得封顶,以极大数 1000000 为封顶,即不封顶
     public function getMeatMaxValue($gambleid) {
-        // return 2; // 肉封顶3点
-        return 1000000;
+        $config = $this->getGambleConfig($gambleid);
+        return $config['meat_max_value'];
     }
-
-
 
     // 让杆的配置,可能返回多条记录,即调整让杆, 受让杆数永远为正数,
     // 如界面为负,则是1人让3人,生成3条记录.
-    public function  getStrokingConfig($gambleid, $userid) {
-
-        // if ($userid == 185) {
-        //     return [
-        //         '185' => [
-        //             '1#' => ['PAR3' => 1, 'PAR4' => 0.5, 'PAR5' => 0.5],
-        //             '3#' => ['PAR3' => 0.5, 'PAR4' => 0.5, 'PAR5' => 0.5]
-        //         ]
-        //     ];
-        // }
+    public function getStrokingConfig($gambleid, $userid) {
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['stroking_config']) && !empty($config['stroking_config'])) {
+            $strokingConfig = json_decode($config['stroking_config'], true);
+            return $strokingConfig[$userid] ?? [];
+        }
 
         return [];
     }
-
-
-
-
 
     /*
     扣分封顶配置,即最多扣多少分,返回正数,小程序界面的
     不封顶为100,因为不可能发生
     */
-    public function  get8421MaxSubValue($gambleid) {
-        // return  3; // 扣2分为止
-        return  10000000; // 不封顶
+    public function get8421MaxSubValue($gambleid) {
+        $config = $this->getGambleConfig($gambleid);
+        return $config['max8421_sub_value'];
     }
 
     /*
@@ -104,67 +96,24 @@ class MRuntimeConfig extends CI_Model {
        3: NoSub ,不扣分
     */
     public function get8421SubConfigString($gambleid) {
-        // Par+2 开始扣分
-        // return "NoSub"; // 不扣分
-        return "Par+4";
+        $config = $this->getGambleConfig($gambleid);
+        return $config['sub8421_config_string'];
     }
-
 
     public function get8421DrawConfig($gambleid) {
-        // return "NoDraw"; // 不考虑顶洞
-        return "Diff_2"; // Indicator差值绝对值在x分以内算顶洞,0即为完全一样,需要交割
+        $config = $this->getGambleConfig($gambleid);
+        return $config['draw8421_config'];
     }
 
-
-    public function  get8421UserAddValuePair($gambleid) {
-        // ------------------
-        // |成绩     | 加分  | 
-        // |"鸟"     |  8   | 
-        // |"帕"     |  4   | 
-        // |"加1     |  2   | 
-        // |"加2"    |  1   | 
-        // |"加3"    |  X   | 
-
-        $data = [
-            67 => [
-                "Par+2" => 1,
-                "Par+1" => 2,
-                "Par" => 4,
-                "Birdie" => 8,
-            ],
-
-            93 => [
-                "Par+2" => 1,
-                "Par+1" => 2,
-                "Par" => 4,
-                "Birdie" => 8,
-            ],
-
-            160 => [
-                "Par+2" => 1,
-                "Par+1" => 2,
-                "Par" => 4,
-                "Birdie" => 8,
-            ],
-
-            185 => [
-                "Par+2" => 2,
-                "Par+1" => 3,
-                "Par" => 4,
-                "Birdie" => 8,
-            ],
-        ];
-
-
-        return $data;
+    public function get8421UserAddValuePair($gambleid) {
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['val8421_config']) && !empty($config['val8421_config'])) {
+            return json_decode($config['val8421_config'], true);
+        }
     }
-
-
-
 
     // 分组方式
     public function getRedBlueConfig($gambleid, $attender_number) {
-
         if ($attender_number == 2) {
             return $this->get2playersRedBlueConfig($gambleid, $attender_number);
         }
@@ -186,96 +135,46 @@ class MRuntimeConfig extends CI_Model {
         return null;
     }
 
-
     // 4人分组方式
     public function get4playersRedBlueConfig($gambleid, $attender_number) {
-        if ($gambleid == 679528) {
-            return "4_乱拉";  //  (1,4 名 ) vs (2,3名),第一洞为 1,4 名,第二洞为 2,3名
-            // return "4_固拉"; //  确定后不改变
-            // return "4_高手不见面";  // (A组 第1名+B组第2名) vs (B组第1+A组第2),防止一方实力太强
-        }
+        $config = $this->getGambleConfig($gambleid);
+        return $config['red_blue_config'];
     }
 
     public function getRanking4TieResolveConfig($gambleid) {
-
-        // A类排序（按成绩）
-        // "score.reverse"                    // A.1: 按成绩排序，冲突时回溯成绩
-        // "score.win_loss.reverse_win"       // A.2.1: 按成绩排序，按输赢，回溯输赢
-        return "score.win_loss.reverse_score";     // A.2.2: 按成绩排序，按输赢，回溯成绩
-
-        // B类排序（按得分）
-        // "indicator.reverse"                // B.1: 按得分排序，冲突时回溯得分
-        // "indicator.win_loss.reverse_win"   // B.2.1: 按得分排序，按输赢，回溯输赢
-        // "indicator.win_loss.reverse_indicator" // B.2.2: 按得分排序，按输赢，回溯得分
-
+        $config = $this->getGambleConfig($gambleid);
+        return $config['ranking_tie_resolve_config'];
     }
-
-
-
 
     public function getAllPlayers($gambleid) {
-        $players = [
-            [
-                'userid' => 93,
-                'username' => 'A为峰_a2',
-                'nickname' => 'A为峰_a2',
-                'cover' => 'http://s1.golf-brother.com/data/attach/user/2014/12/29/c240_cb01d5ff2b7ec41ebeab07c461e191aa.jpg',
-                'is_attender' => true,
-                'initial_team' => 'blue',
-                'skill_level' => 1, // 技术水平排名
-            ],
-            [
-                'userid' => 67,
-                'username' => '不发力',
-                'nickname' => '不发力',
-                'cover' => 'http://s1.golf-brother.com/data/attach/userVipPic/2023/10/12/4e80dfffdc44daf29ec46b35a93ef6a0.png',
-                'is_attender' => true,
-                'initial_team' => 'red',
-                'skill_level' => 2,
-            ],
-            [
-                'userid' => 185,
-                'username' => 'A图图手机',
-                'nickname' => 'A图图手机',
-                'cover' => 'http://s1.golf-brother.com/data/attach/userVipPic/2023/05/14/c240_c93a158495a41393d4799324c952cea1.png',
-                'is_attender' => true,
-                'initial_team' => 'blue',
-                'skill_level' => 3,
-            ],
-            [
-                'userid' => 160,
-                'username' => 'A高攀_a1',
-                'nickname' => 'A高攀_a1',
-                'cover' => 'http://s1.golf-brother.com/data/attach/user/2014/10/15/97e3c9ba9b58bb48c08ab6871decde0f.png',
-                'is_attender' => true,
-                'initial_team' => 'red',
-                'skill_level' => 4,
-            ]
-        ];
-        return $players;
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['all_players']) && !empty($config['all_players'])) {
+            return json_decode($config['all_players'], true);
+        }
     }
-
 
     // 获取出发顺序  
     public function getBootStrapOrder($gambleid) {
-        return [185, 93, 160, 67];
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['bootstrap_order']) && !empty($config['bootstrap_order'])) {
+            return json_decode($config['bootstrap_order'], true);
+        }
     }
 
     public function getAttenders($gambleid) {
-        return [185, 93, 160, 67];
+        $config = $this->getGambleConfig($gambleid);
+        if (isset($config['attenders']) && !empty($config['attenders'])) {
+            return json_decode($config['attenders'], true);
+        }
     }
 
     /* 包负分配置
     * 1: 不包负分       NODUTY
     * 2: 包负分         DUTY_NEGATIVE
     * 3: 同伴顶头保负分  DUTY_CODITIONAL
-    *
-    *
-    *
-    *
     */
     public function getDutyConfig($gambleid) {
-        // return  "DUTY_NEGATIVE";
-        return "DUTY_CODITIONAL";
+        $config = $this->getGambleConfig($gambleid);
+        return $config['duty_config'];
     }
 }
