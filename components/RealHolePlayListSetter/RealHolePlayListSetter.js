93d48d71959dadaeebee91ea2c3782c88378a9b7
4python@ai <4python@ai>
2025-07-17 15:52:42 +0800

    环形球洞算法

--- components/RealHolePlayListSetter/RealHolePlayListSetter.js
+++ components/RealHolePlayListSetter/RealHolePlayListSetter.js
@@ -6,7 +6,7 @@ Component({
     properties: {
         holeList: {
             type: Array,
-            value: Array.from({ length: 18 }, (_, i) => i + 1),
+            value: []
         },
         showModal: {
             type: Boolean,
@@ -22,24 +22,29 @@ Component({
         },
     },
 
-    data: {},
+    data: { selectedIndex: null, holePlayList: [] },
     lifetimes: {
         attached() {
             console.log('[RealHolePlayListSetter] attached', this.properties);
+            this.setData({ holePlayList: this.properties.holeList });
+
         }
     },
     observers: {
         showModal(newVal) {
-            console.log('[RealHolePlayListSetter] showModal changed:', newVal);
+            // console.log('[RealHolePlayListSetter] showModal changed:', newVal);
         },
+
         modalType(newVal) {
-            console.log('[RealHolePlayListSetter] modalType changed:', newVal);
+            // console.log('[RealHolePlayListSetter] modalType changed:', newVal);
         },
+
         selectedIndex(newVal) {
-            console.log('[RealHolePlayListSetter] selectedIndex changed:', newVal);
+            // console.log('[RealHolePlayListSetter] selectedIndex changed:', newVal);
         },
+
         holeList(newVal) {
-            console.log('[RealHolePlayListSetter] holeList changed:', newVal);
+            // console.log('[RealHolePlayListSetter] holeList changed:', newVal);
         }
     },
 
@@ -50,13 +55,32 @@ Component({
         },
 
         onSelectHole(e) {
-            const idx = e.currentTarget.dataset.index;
-            console.log('[RealHolePlayListSetter] onSelectHole', idx);
-            this.setData({ selectedIndex: idx });
+            const hindex = Number(e.currentTarget.dataset.hindex);
+            const { holeList } = this.properties;
+
+            // debug: 打印所有 holeList
+            console.log('[onSelectHole] 当前holeList:', holeList);
+            // debug: 打印当前点击的 hindex
+            console.log('[onSelectHole] 当前点击hindex:', hindex);
+
+            // 1. 先按 hindex 升序排列
+            const sortedList = [...holeList].sort((a, b) => (a.hindex || 0) - (b.hindex || 0));
+            // 2. 找到点击的 hindex 在排序后数组中的下标
+            const startIdx = sortedList.findIndex(hole => Number(hole.hindex) === hindex);
+            // 3. 用这个下标做环形切片
+            const newHolePlayList = sortedList.slice(startIdx).concat(sortedList.slice(0, startIdx));
+
+            this.setData({
+                selectedIndex: hindex,
+                holePlayList: newHolePlayList
+            });
+
+            console.log('[onSelectHole] 新holePlayList:', newHolePlayList);
+            console.log('[onSelectHole] 当前selectedIndex(hindex):', hindex);
         },
 
         onConfirm() {
-            console.log('[RealHolePlayListSetter] onConfirm', this.properties.modalType, this.data.selectedIndex);
+            console.log('确定:::', this.properties.modalType, this.data.selectedIndex);
             this.triggerEvent('change', {
                 modalType: this.properties.modalType,
                 selectedIndex: this.data.selectedIndex !== undefined ? this.data.selectedIndex : this.properties.selectedIndex
