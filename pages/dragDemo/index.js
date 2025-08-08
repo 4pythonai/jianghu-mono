22f9286322fa308c3c14b8d0e41e30b6f4429c58
4python@ai <4python@ai>
2025-08-09 00:58:14 +0800

    style

--- pages/dragDemo/index.js
+++ pages/dragDemo/index.js
@@ -1,24 +1,24 @@
 const app = getApp()
 
 let listData = [
-	{ hindex: 0, holename: "第1洞" },
-	{ hindex: 1, holename: "第2洞" },
-	{ hindex: 2, holename: "第3洞" },
-	{ hindex: 3, holename: "第4洞" },
-	{ hindex: 4, holename: "第5洞" },
-	{ hindex: 5, holename: "第6洞" },
-	{ hindex: 6, holename: "第7洞" },
-	{ hindex: 7, holename: "第8洞" },
-	{ hindex: 8, holename: "第9洞" },
-	{ hindex: 9, holename: "第10洞" },
-	{ hindex: 10, holename: "第11洞" },
-	{ hindex: 11, holename: "第12洞" },
-	{ hindex: 12, holename: "第13洞" },
-	{ hindex: 13, holename: "第14洞" },
-	{ hindex: 14, holename: "第15洞" },
-	{ hindex: 15, holename: "第16洞" },
-	{ hindex: 16, holename: "第17洞" },
-	{ hindex: 17, holename: "第18洞" }
+	{ hindex: 0, holename: "A1" },
+	{ hindex: 1, holename: "A2" },
+	{ hindex: 2, holename: "A3" },
+	{ hindex: 3, holename: "A4" },
+	{ hindex: 4, holename: "A5" },
+	{ hindex: 5, holename: "A6" },
+	{ hindex: 6, holename: "A7" },
+	{ hindex: 7, holename: "A8" },
+	{ hindex: 8, holename: "A9" },
+	{ hindex: 9, holename: "B1" },
+	{ hindex: 10, holename: "B2" },
+	{ hindex: 11, holename: "B3" },
+	{ hindex: 12, holename: "B4" },
+	{ hindex: 13, holename: "B5" },
+	{ hindex: 14, holename: "B6" },
+	{ hindex: 15, holename: "B7" },
+	{ hindex: 16, holename: "B8" },
+	{ hindex: 17, holename: "B9" }
 ];
 
 Page({
