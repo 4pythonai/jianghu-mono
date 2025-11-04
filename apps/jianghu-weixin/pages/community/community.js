119ff4238fdf7ec3f0efa807e589e0e4e124ae07
4python@ai <4python@ai>
2025-11-04 16:35:29 +0800

    礼仪入门

--- apps/jianghu-weixin/pages/community/community.js
+++ apps/jianghu-weixin/pages/community/community.js
@@ -1,5 +1,13 @@
 // community.js
 Page({
-    data: {},
-    onLoad() { }
+    data: {
+        pageTitle: '高尔夫礼仪入门'
+    },
+
+    onLoad() {
+        // 设置页面标题
+        wx.setNavigationBarTitle({
+            title: '江湖'
+        });
+    }
 })
